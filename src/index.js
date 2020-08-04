d3705d64e5f6b9eabadc2c3fa9927be04f74543d
lbstc888@163.com <lbstc888@163.com>
2020-08-04 11:54:18 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import nice from './nice.png';
+import data from './data.xml';
+
 
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var image = new Image();
+  image.src = nice;
+
+  element.appendChild(image);
+  console.log(data);
   return element;
 }
 
 document.body.appendChild(component());
-// npm webpack
+// 在javascript模块中 import 一个css文件需要添加style-loader和css-loader
+// 图片使用file-loader
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,45 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader', 
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            }
+          }
+        ]
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }, {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 };
 
