f7f399ffc66208c45592a197991c73ec5359460c
lbstc888@163.com <lbstc888@163.com>
2020-08-06 11:39:33 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
+import { cube } from './math'
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('pre');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack',
+    '5 cubes is equal to ' + cube(5)
+  ].join('\n\n');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
   return element;
 }
 
