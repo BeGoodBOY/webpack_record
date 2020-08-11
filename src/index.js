9543e3de505b9855411aa7370a41e7fe3364b07e
lbstc888@163.com <lbstc888@163.com>
2020-08-11 17:58:46 +0800

    shimming

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import _ from 'lodash';
+import {file, parse} from './globals.js'
 
 function component() {
   var element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  //确认this指向window
+  // this.alert('this is window');
+  // parse();
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      // {
+      //   test: require.resolve('./src/index.js'),
+      //   use: 'imports-loader?this=>window'
+      // },
+      // {
+      //   test: require.resolve('./src/globals.js'),
+      //   use: 'exports-loader?file,parse=helpers.parse'
+      // }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+      // 获取单个方法用数组路径
+      join: ['lodash', 'join']
+    })
+  ]
 };
 
-// npx webpack --config webpack.config.js
